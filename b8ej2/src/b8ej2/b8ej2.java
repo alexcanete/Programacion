30d33b66feb3724127c6930d2211d5b644430bec
alex <alex@canete.org>
2021-01-19 10:11:25 +0100

    a

---
+++ b8ej2/src/b8ej2/b8ej2.java
@@ -0,0 +1,12 @@
+package b8ej2;
+
+public class b8ej2 {
+
+	public static void main(String[] args) {
+		clase cBiologia = new clase("Biologia",2000,(byte) 2);
+		
+		System.out.println("La clase "+cBiologia.getsNombre()+" con id "+cBiologia.getiCodigo()+" y curso "+cBiologia.getbCurso());
+
+	}
+
+}
