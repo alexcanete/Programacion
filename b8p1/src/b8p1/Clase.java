1884941b89ca681bf06eadf3afadb56402bf2f79
alex <alex@canete.org>
2021-01-20 13:27:17 +0100

    actualizacion

--- b8p1/src/b8p1/Clase.java
+++ b8p1/src/b8p1/Clase.java
@@ -2,4 +2,32 @@ package b8p1;
 
 public class Clase {
 
+	private byte bDia;
+	private byte bMes;
+	private short shAño;
+	
+	
+	
+	
+	
+	public byte getbDia() {
+		return bDia;
+	}
+	public void setbDia(byte bDia) {
+		this.bDia = bDia;
+	}
+	public byte getbMes() {
+		return bMes;
+	}
+	public void setbMes(byte bMes) {
+		this.bMes = bMes;
+	}
+	public short getShAño() {
+		return shAño;
+	}
+	public void setShAño(short shAño) {
+		this.shAño = shAño;
+	}
+	
+	
 }
