0b5f6ece09a03e68669e35c9d3a10d65c5bc47da
alex <alex@canete.org>
2021-01-21 12:51:20 +0100

    actuaizacion

--- b8p2/src/b8p2/Punto.java
+++ b8p2/src/b8p2/Punto.java
@@ -2,4 +2,48 @@ package b8p2;
 
 public class Punto {
 
+	private double dX, dY;
+	
+	public Punto (double dX, double dY) {
+		setdX(dX);
+		setdY(dY);
+	}
+
+	public void borrar() {
+		setdX(0);
+		setdY(0);
+	}
+	
+	public void mover(double dX) {
+		this.dX=dX;
+		this.dY=dY;
+	}
+	
+	public void mover(Punto punto) {
+		this.dX= punto.getdX();
+		this.dY= punto.getdY();
+	}
+	
+	public double getdX() {
+		return dX;
+	}
+
+	public void setdX(double dX) {
+		this.dX = dX;
+	}
+
+	public double getdY() {
+		return dY;
+	}
+
+	public void setdY(double dY) {
+		this.dY = dY;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Punto [dX=" + dX + ", dY=" + dY + "]";
+	}
 }
--- b8p2/src/b8p2/b8p2.java
+++ b8p2/src/b8p2/b8p2.java
@@ -4,7 +4,8 @@ public class b8p2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Punto pCero = new Punto (0, 0);
+		System.out.println(pCero);
 	}
 
 }
