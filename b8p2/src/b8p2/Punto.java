28835f2731d241952032259189bedc9a6228cd44
alex <alex@canete.org>
2021-01-21 11:53:51 +0100

    p3

---
+++ b8p2/src/b8p2/Punto.java
@@ -0,0 +1,5 @@
+package b8p2;
+
+public class Punto {
+
+}
---
+++ b8p2/src/b8p2/b8p2.java
@@ -0,0 +1,10 @@
+package b8p2;
+
+public class b8p2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
