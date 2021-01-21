bcd5a83770e327114da9ea51cfe16e96faba6d49
alex <alex@canete.org>
2021-01-21 11:24:52 +0100

    actualizacion p2

--- b8p1/src/b8p1/Clase.java
+++ b8p1/src/b8p1/Clase.java
@@ -2,17 +2,13 @@ package b8p1;
 
 public class Clase {
 
-	private byte bDia;
-	private byte bMes;
+	private byte bDia, bMes;
 	private short shAnio;
 	
-	public Clase (byte bDia) {
-		setbDia(bDia);
-	}
-	
-	public Clase (byte bMes, short shanio) {
-		setbMes(bMes);
+	public Clase (byte bDia, byte bMes, short shAnio) {
 		setshAnio(shAnio);
+		setbMes(bMes);
+		setbDia(bDia);
 	}
 
 	public byte getbDia() {
@@ -20,10 +16,38 @@ public class Clase {
 	}
 
 	public void setbDia(byte bDia) {
-		if (bDia<=31) {
-			this.bDia = bDia;
+		if(boBisiesto(this.getShAnio()) && this.getbMes() == 2) {
+			if(bDia < 30) {
+				this.bDia = bDia;
+			}
 		}
-		
+		else if(!boBisiesto(this.getShAnio()) && this.getbMes() == 2) {
+			if(bDia < 29) {
+				this.bDia = bDia;
+			}
+		}
+		else {
+			if(this.getbMes() % 2 != 0 && this.getbMes() <= 7) {
+				if(bDia < 32) {
+					this.bDia = bDia;
+				}
+			}
+			else if(this.getbMes() % 2 == 0 && this.getbMes() > 7) {
+				if(bDia < 32) {
+					this.bDia = bDia;
+				}
+			}
+			else if(this.getbMes() % 2 == 0 && this.getbMes() < 7) {
+				if(bDia < 31) {
+					this.bDia = bDia;
+				}
+			}
+			else if(this.getbMes() % 2 != 0 && this.getbMes() > 7) {
+				if(bDia < 31) {
+					this.bDia = bDia;
+				}
+			}
+		}
 	}
 
 	public byte getbMes() {
@@ -31,10 +55,9 @@ public class Clase {
 	}
 
 	public void setbMes(byte bMes) {
-		if (bMes<=12) {
+		if (bMes > 0 && bMes<=12) {
 			this.bMes = bMes;
 		}
-		
 	}
 
 	public short getShAnio() {
@@ -42,14 +65,31 @@ public class Clase {
 	}
 
 	public void setshAnio(short shAnio) {
-		if ((shAnio % 4 == 0 && shAnio % 100 != 0) || (shAnio % 100 == 0 && shAnio % 400 == 0)) {
+		if(shAnio >= 0) {
 			this.shAnio = shAnio;
 		}
-		
 	}
 	
+	public boolean boBisiesto(short shAnio){
+		boolean boBisiesto=false;
+		if ((shAnio % 4 == 0 && shAnio % 100 != 0) || (shAnio % 100 == 0 && shAnio % 400 == 0)) {
+			boBisiesto= true;
+		}
+		return boBisiesto;
+	}
 	
+		
 	
+
 	
-	
+	public String toString() {
+		return "Es el " + this.getbDia() + " del " + this.getbMes() + " del año " + this.getShAnio() ;
+	}
 }
+	
+	
+	
+	
+	
+	
+
--- b8p1/src/b8p1/b8p1.java
+++ b8p1/src/b8p1/b8p1.java
@@ -1,10 +1,13 @@
 package b8p1;
 
+
 public class b8p1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Clase cFecha = new Clase((byte)31, (byte)8,(short) 2020);
+		
+		System.out.println(cFecha);
 	}
 
 }
