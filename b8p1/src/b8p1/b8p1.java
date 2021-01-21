ca102c43d4047a58cc2d7cb6cda4f2baa5aac56a
alex <alex@canete.org>
2021-01-21 11:25:50 +0100

    actualizacion p2

--- b8p1/src/b8p1/b8p1.java
+++ b8p1/src/b8p1/b8p1.java
@@ -5,7 +5,7 @@ public class b8p1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Clase cFecha = new Clase((byte)31, (byte)8,(short) 2020);
+		Clase cFecha = new Clase((byte)29, (byte)2,(short) 2020);
 		
 		System.out.println(cFecha);
 	}
