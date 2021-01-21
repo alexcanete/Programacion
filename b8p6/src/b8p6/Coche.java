95c37079942a2c3e046cc00a168a1f355416826a
Alex Cañete <alex@canete.org>
2021-01-21 20:08:54 +0100

    b8p6

---
+++ b8p6/src/b8p6/Coche.java
@@ -0,0 +1,10 @@
+package b8p6;
+
+public class Coche {
+
+	private String sModelo, sColor, sTipo, sSeguro, sMatricula;
+	private short shAnio;
+	private boolean boMetalizada;
+	
+	
+}
---
+++ b8p6/src/b8p6/b8p6.java
@@ -0,0 +1,10 @@
+package b8p6;
+
+public class b8p6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
