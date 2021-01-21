496a1905a4cfb9890ab78337b2b77dc3c9073b96
Alex Cañete <alex@canete.org>
2021-01-21 22:01:32 +0100

    actualizacion

--- b8p7/src/b8p7/Coche.java
+++ b8p7/src/b8p7/Coche.java
@@ -2,4 +2,84 @@ package b8p7;
 
 public class Coche {
 
+	private String sModelo, sColor, sTipo, sSeguro, sMatricula;
+	private short shAnio;
+	private boolean boMetalizada;
+	
+	public Coche (String sModelo, String sColor, String sTipo, String sSeguro, String sMatricula, short shAnio, boolean boMetalizado){
+		setsModelo(sModelo);
+		setsColor(sColor);
+		setsTipo(sTipo);
+		setsSeguro(sSeguro);
+		setsMatricula(sMatricula);
+		setShAnio(shAnio);
+		setBoMetalizada(boMetalizado);
+		
+	}
+
+	public String getsModelo() {
+		return sModelo;
+	}
+
+	public void setsModelo(String sModelo) {
+		this.sModelo = sModelo;
+	}
+
+	public String getsColor() {
+		return sColor;
+	}
+
+	public void setsColor(String sColor) {
+		this.sColor = sColor;
+	}
+
+	public String getsTipo() {
+		return sTipo;
+	}
+
+	public void setsTipo(String sTipo) {
+		this.sTipo = sTipo;
+	}
+
+	public String getsSeguro() {
+		return sSeguro;
+	}
+
+	public void setsSeguro(String sSeguro) {
+		this.sSeguro = sSeguro;
+	}
+
+	public String getsMatricula() {
+		return sMatricula;
+	}
+
+	public void setsMatricula(String sMatricula) {
+		this.sMatricula = sMatricula;
+	}
+
+	public short getShAnio() {
+		return shAnio;
+	}
+
+	public void setShAnio(short shAnio) {
+		this.shAnio = shAnio;
+	}
+
+	public boolean isBoMetalizada() {
+		return boMetalizada;
+	}
+
+	public void setBoMetalizada(boolean boMetalizada) {
+		this.boMetalizada = boMetalizada;
+	}
+	 public void imprimeCoche(String sModelo, String sColor) {
+		 System.out.println(this.sModelo +this.sColor);
+	 }
+
+	@Override
+	public String toString() {
+		return "Coche [sModelo=" + sModelo + ", sColor=" + sColor + ", sTipo=" + sTipo + ", sSeguro=" + sSeguro
+				+ ", sMatricula=" + sMatricula + ", shAnio=" + shAnio + ", boMetalizada=" + boMetalizada + "]";
+	}
+	 
 }
--- b8p7/src/b8p7/b8p7.java
+++ b8p7/src/b8p7/b8p7.java
@@ -1,5 +1,11 @@
 package b8p7;
 
 public class b8p7 {
-
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String sModelo, String sColor, String sTipo, String sSeguro, String sMatricula, short shAnio, boolean boMetalizado
+		Coche cCoche= new Coche("Ford", "Negro", "mini", "Todo riesgo", "ABC123", (short)2018, true);
+		System.out.println(cCoche);
+	}
 }
