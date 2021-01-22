5093eef20457a72bed5995c142a730379767e367
alex <alex@canete.org>
2021-01-22 11:21:39 +0100

    b8p7

--- b8p7/src/b8p7/b8p7.java
+++ b8p7/src/b8p7/b8p7.java
@@ -6,6 +6,7 @@ public class b8p7 {
 		// TODO Auto-generated method stub
 		//String sModelo, String sColor, String sTipo, String sSeguro, String sMatricula, short shAnio, boolean boMetalizado
 		Coche cCoche= new Coche("Ford", "Negro", "mini", "Todo riesgo", "ABC123", (short)2018, true);
+		System.out.println(cCoche);
 		cCoche.imprimeCoche();
 	}
 }
