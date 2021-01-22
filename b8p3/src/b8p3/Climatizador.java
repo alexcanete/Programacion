34378162531b589e0b27b4041dd9a1693c9255f4
alex <alex@canete.org>
2021-01-22 14:08:32 +0100

    b8p3

--- b8p3/src/b8p3/Climatizador.java
+++ b8p3/src/b8p3/Climatizador.java
@@ -1,5 +1,46 @@
 package b8p3;
 
 public class Climatizador {
+	private boolean booEncendido;
+	private float fTActual, fTDeseada;
+	
+	public Climatizador (boolean booEncendido, float fTActual, float fTDeseada) {
+		setBooEncendido(booEncendido);
+		setfTActual(fTActual);
+		setfTDeseada(fTDeseada);
+		
+	}
 
+	public boolean isBooEncendido() {
+		return booEncendido;
+	}
+
+	public void setBooEncendido(boolean booEncendido) {
+		this.booEncendido = booEncendido;
+	}
+
+	public float getfTActual() {
+		return fTActual;
+	}
+
+	public void setfTActual(float fTActual) {
+		this.fTActual = fTActual;
+	}
+
+	public float getfTDeseada() {
+		return fTDeseada;
+	}
+
+	public void setfTDeseada(float fTDeseada) {
+		this.fTDeseada = fTDeseada;
+	}
+
+	@Override
+	public String toString() {
+		return "Climatizador [booEncendido=" + booEncendido + ", fTActual=" + fTActual + ", fTDeseada=" + fTDeseada
+				+ "]";
+	}
+	public void enfriar() {
+		;
+	}
 }
--- b8p3/src/b8p3/b8p3.java
+++ b8p3/src/b8p3/b8p3.java
@@ -4,7 +4,9 @@ public class b8p3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//boolean booEncendido, float fTActual, float fTDeseada
+		Climatizador cClimatizador= new Climatizador((boolean)true, (float)19.0, (float)24.0);
+		System.out.println(cClimatizador);
 	}
 
 }
