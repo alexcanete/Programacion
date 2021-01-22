2642fbcf36d856034fc32bfd9636b57a7799a470
alex <alex@canete.org>
2021-01-22 14:24:55 +0100

    actualizacion

--- b8p3/src/b8p3/b8p3.java
+++ b8p3/src/b8p3/b8p3.java
@@ -7,6 +7,7 @@ public class b8p3 {
 		//boolean booEncendido, float fTActual, float fTDeseada
 		Climatizador cClimatizador= new Climatizador((boolean)true, (float)19.0, (float)24.0);
 		System.out.println(cClimatizador);
+		
 	}
 
 }
