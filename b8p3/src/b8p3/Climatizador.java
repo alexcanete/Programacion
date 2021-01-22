be97507591e1cd268661927b17ef80d3c758efda
alex <alex@canete.org>
2021-01-22 12:49:00 +0100

    b8p3

---
+++ b8p3/src/b8p3/Climatizador.java
@@ -0,0 +1,5 @@
+package b8p3;
+
+public class Climatizador {
+
+}
---
+++ b8p3/src/b8p3/b8p3.java
@@ -0,0 +1,10 @@
+package b8p3;
+
+public class b8p3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
