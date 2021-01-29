0e48a6aea096fc92f500bd82ce5172f01b725dcc
alex <alex@canete.org>
2021-01-29 14:22:26 +0100

    subida

--- b9ej3/src/b9ej3/Operar.java
+++ b9ej3/src/b9ej3/Operar.java
@@ -1,5 +1,21 @@
 package b9ej3;
 
 public class Operar {
+	private double dNumero;
 
+	public Operar(double dNumero) {
+		 
+	}
+	
+	public double getdNumero() {
+		return dNumero;
+	}
+
+	public void setdNumero(double dNumero) {
+		this.dNumero = dNumero;
+	}
+	public int setInt(double dNumero) {
+		return (int)dNumero;
+	}
+	
 }
