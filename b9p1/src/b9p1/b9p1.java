50f0391b8dff21f8aa30425dfb092834aa609247
Alex Cañete <alex@canete.org>
2021-01-31 22:49:48 +0100

    subida

---
+++ b9p1/src/b9p1/b9p1.java
@@ -0,0 +1,12 @@
+package b9p1;
+
+public class b9p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		reloj oReloj = new reloj((byte)1,(byte) 2,(byte) 3);
+		System.out.print(reloj.toString());
+	}
+
+}
