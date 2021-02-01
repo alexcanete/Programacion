987a98f226f7cd41d6b17fbc74fd4cfbdde86971
alex <alex@canete.org>
2021-02-01 14:02:20 +0100

    .

---
+++ b10ej1/src/b10ej1/b10ej1.java
@@ -0,0 +1,10 @@
+package b10ej1;
+
+public class b10ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ b10ej1/src/b10ej1/b10ej1I.java
@@ -0,0 +1,8 @@
+package b10ej1;
+
+public interface b10ej1I {
+	public final int cinco = 5;
+	public final float pi = 3.1416F;
+	public float longitudCircunferencia(float radio);
+	public int sumaCinco(int numero);
+}
