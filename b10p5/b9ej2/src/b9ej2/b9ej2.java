ab0389b11cb04f814517a9ded96348a2c4015187
alex <alex@canete.org>
2021-02-05 12:43:21 +0100

    b10p5

---
+++ b10p5/b9ej2/src/b9ej2/b9ej2.java
@@ -0,0 +1,13 @@
+package b9ej2;
+
+public class b9ej2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String sNombre, String sApellido1, String sApellido2
+		Persona oPersona = new Persona("Alex", "Canete","hajiphylakti" );
+		
+		//oPersona.sApellido1= "Perez"; asi violo a la clase
+	}
+
+}
