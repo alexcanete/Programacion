24c8cfe5e2cd6437d17248fb88ac427daf8e11f3
alex <alex@canete.org>
2021-02-05 12:57:45 +0100

    .

--- b1p2/src/b1p2/b1p2.java
+++ b1p2/src/b1p2/b1p2.java
@@ -5,6 +5,8 @@ public class b1p2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Movil oMovil= new Movil(654765765);
+		Fijo oFijo= new Fijo(954876456);
+		Satelite oSatelite= new Satelite(675435789);
 				
 	}
 }
