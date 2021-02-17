e59669a5cad178babe7b425ef64619707bf3195f
alex <alex@canete.org>
2021-02-17 12:14:17 +0100

    .

--- b11ej1/ReproductorPortatil.java
+++ b11ej1/ReproductorPortatil.java
@@ -1,6 +1,163 @@
-public class ReproductorPortatil {
-    private String sMarca, sModelo;
+public class ReproductorPortatil implements IReproductorPortatil{
+    private String sMarca, sModelo, sTipoDeAlmacenamiento, sCapacidadDeAlmacenamiento, sPantalla, sTipoDeBacteria;
+    private boolean boReproduceSonido, boReproduceVideo;
+    private byte bAutonomia;
+    private float fPeso, fAncho, fAlto, fGrosor;    
+
+
+    @Override
+    public boolean getboReproduceSonido() {
+        return boReproduceSonido;
+    }
+
+    @Override
+    public void setboReproduceSonido(boolean boReproduceSonido) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean getboReproduceVideo() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void setboReproduceVideo(boolean boReproduceVideo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getsTipoDeAlmacenamiento() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setsTipoDeAlmacenamiento(String sTipoDeAlmacenamiento) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int getciCapacidadDeAlmacenamiento() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setiCapacidadDeAlmacenamiento(int iCapacidadDeAlmacenamiento) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getsPantalla() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setsPantalla(String sPantalla) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getsTipoDeBacteria() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setsTipoDeBacteria(String sTipoDeBacteria) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getsMarca() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setsMarca(String sMarca) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getsModelo() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setsModelo(String sModelo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public byte getbAutonomia() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setsbAutonomia(byte bAutonomia) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public float getfPeso() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setfPeso(float fPeso) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public float getfAncho() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setfAncho(float fAncho) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public float getfAlto() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setfAlto(float fAlto) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public float getfGrosor() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setfGrosor(float fGrosor) {
+        // TODO Auto-generated method stub
+
+    }
     
-    public boolean
-	
 	}
